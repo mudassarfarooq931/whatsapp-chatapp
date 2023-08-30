0409641bce2f36ce55304f71242db18a5c7b1a66
mudassar <mudassarfarooq931@gmail.com>
2023-08-30 22:23:49 +0500

    commit_check

--- src/screens/Calls.js
+++ src/screens/Calls.js
@@ -1,12 +1,12 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import {View, Text} from 'react-native';
+import React from 'react';
 
 const Calls = () => {
   return (
     <View>
-      <Text>Calls</Text>
+      <Text>Callsss</Text>
     </View>
-  )
-}
+  );
+};
 
-export default Calls
+export default Calls;
